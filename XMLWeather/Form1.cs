aad1776531a224f39afd8d4f03a50699a9fb3bd1
edwaangu <edwaangu171@outlook.com>
2021-10-22 11:05:22 -0400

    yeah

--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -13,8 +13,7 @@ namespace XMLWeather
 {
     public partial class Form1 : Form
     {
-        // TODO: create list to hold day objects
-
+        public static List<Day> days = new List<Day>();
 
         public Form1()
         {
@@ -34,12 +33,23 @@ namespace XMLWeather
 
             while (reader.Read())
             {
-                //TODO: create a day object
+                Day d = new Day();
 
-                //TODO: fill day object with required data
+                reader.ReadToFollowing("time");
+                d.date = reader.GetAttribute("day");
 
-                //TODO: if day object not null add to the days list
+
+                reader.ReadToFollowing("temperature");
+                d.tempLow = reader.GetAttribute("min");
+                d.tempHigh = reader.GetAttribute("max");
+
+                if (d != null)
+                {
+                    days.Add(d);
+                }
             }
+
+            reader.Close();
         }
 
         private void ExtractCurrent()
@@ -48,7 +58,18 @@ namespace XMLWeather
             XmlReader reader = XmlReader.Create("http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0");
 
             //TODO: find the city and current temperature and add to appropriate item in days list
+            reader.ReadToFollowing("city");
+            days[0].location = reader.GetAttribute("name");
 
+            reader.ReadToFollowing("temperature");
+            days[0].currentTemp = reader.GetAttribute("value");
+            days[0].tempLow = reader.GetAttribute("min");
+            days[0].tempHigh = reader.GetAttribute("max");
+
+            reader.ReadToFollowing("lastupdate");
+            days[0].currentTime = reader.GetAttribute("value");
+
+            reader.Close();
         }
 
 
